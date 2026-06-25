a9b234bbf274746e8a2cde599224d8841fbdb7c1
Mathilde Ripart <mathilde.ripart@kcl.ac.uk>
2026-06-25 12:32:18 +0100

    add function to get the brain mask

--- clinmriqc/general.py
+++ clinmriqc/general.py
@@ -8,3 +8,18 @@ def load_nifti (path:str) -> np.ndarray:
     img = nibabel.load(path)
     data = np.asanyarray(img.dataobj, dtype = np.float32)
     return data 
+
+
+def get_brain_mask(path:str, outfile=None) -> np.ndarray: 
+    import brainchop as bc
+    # Load, skull-strip, save
+    vol = bc.load(path)
+    brain_stripped = bc.segment(vol, "mindgrab")
+    mask = (brain_stripped.data.numpy()>0).astype(bool)
+    if not outfile is None:
+        bc.save(mask, outfile)
+        print(f'Saved brain mask in {outfile}')
+    return mask
+
+
+# %%
